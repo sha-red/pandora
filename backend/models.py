b1d4411dc2243ec9a0064180d92cf93e69f89314
j <0x006A@0x2620.org>
2009-09-07 17:43:42 +0200

    add south migrations

--- backend/models.py
+++ backend/models.py
@@ -719,7 +719,7 @@ class File(models.Model):
 class Archive(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
-    name = models.CharField(max_length=512, unique=True)
+    name = models.CharField(max_length=255, unique=True)
     users = models.ManyToManyField(User, related_name='archives')
 
 class ArchiveFile(models.Model):
