cfc913723ca689095d4a272426656cbd9c9d5c46
j <0x006A@0x2620.org>
2010-01-25 20:28:39 +0530

    add encoder

---
+++ oxdb/backend/management/commands/encoder.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# vi:si:et:sw=4:sts=4:ts=4
+
+import os
+from os.path import join, dirname, basename, splitext, exists
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from ... import encoder
+
+
+class Command(BaseCommand):
+    """
+    listen to rabbitmq and execute encoding tasks.
+    """
+    help = 'listen to rabbitmq and execute encoding tasks.'
+    args = ''
+
+    def handle(self, **options):
+        encoder.run()
+
