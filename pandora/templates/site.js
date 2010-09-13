2ad4c5eba017bca7ef8970aa8e31c4cba6127079
j <0x006A@0x2620.org>
2010-09-13 17:45:06 +0200

    move site into pandora.json

--- pandora/templates/site.js
+++
@@ -1,9 +0,0 @@
-var site = {};
-site.url = "{{settings.URL}}";
-site.name = "{{settings.SITENAME}}";
-site.id = "{{settings.SITEID}}";
-
-site.pages = {};
-{% for page in pages %}
-site.pages['{{page.name}}'] = '{{page.body|escapejs}}';
-{% endfor %}
