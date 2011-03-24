29397141543ef245e7e3f9670a754c987475e7c6
j <0x006A@0x2620.org>
2011-03-24 11:39:35 +0100

    use django 1.3.x

--- pandora/settings.py
+++ pandora/settings.py
@@ -30,13 +30,14 @@ DEFAULT_FROM_EMAIL='admin@' + URL.split('/')[0]
 
 MANAGERS = ADMINS
 
-DATABASE_ENGINE = 'sqlite3'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'dev.sqlite'             # Or path to database file if using sqlite3.
-DATABASE_USER = ''             # Not used with sqlite3.
-DATABASE_PASSWORD = ''         # Not used with sqlite3.
-DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
-
+DATABASES = {
+    'default': {
+        'NAME': 'pandora',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'USER': 'pandora',
+        'PASSWORD': ''
+    }
+}
 
 #rabbitmq connection settings
 CELERY_RESULT_BACKEND = "database"
