6c1216017457ec8b3cddcfb0ca8b1b2ef593ddd0
j <0x006A@0x2620.org>
2011-09-04 20:35:50 +0200

    silence build.py

--- update.sh
+++ update.sh
@@ -3,6 +3,7 @@ base=`pwd`
 bzr pull http://code.0x2620.org/pandora/
 cd static/oxjs
 bzr pull http://code.0x2620.org/oxjs/
-./tools/build/build.py
+echo build oxjs
+./tools/build/build.py >/dev/null
 cd $base
 test -e src/python-ox && cd src/python-ox && bzr pull http://code.0x2620.org/python-ox/
