188dc1a6ab2202e775c7b61c5938782346fd7e1e
rolux <rolux@rollux.org>
2011-10-14 00:13:42 +0000

    fix a regression with the info part of the icon text in grid view, when sorting by director or title

--- static/js/pandora/ui/list.js
+++ static/js/pandora/ui/list.js
@@ -149,11 +149,10 @@ pandora.ui.list = function() {
                         ui.icons == 'posters'
                         ? (ui.showSitePoster ? 'siteposter' : 'poster') : 'icon'
                     ) + size + '.jpg',
-                    format, info, sortKey;
+                    format, info, sortKey = sort[0].key;
                 if (['title', 'director'].indexOf(sortKey) > -1) {
                     info = data['year'];
                 } else {
-                    sortKey = sort[0].key;
                     format = pandora.getSortKeyData(sortKey).format;
                     info = format
                         ? Ox['format' + Ox.toTitleCase(format.type)]
