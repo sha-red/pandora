d953debf09c75bfa64a21cdbd2c9597ae3dd258e
j <0x006A@0x2620.org>
2011-10-20 11:09:22 +0200

    fix rebuildcache

--- pandora/item/management/commands/rebuildcache.py
+++ pandora/item/management/commands/rebuildcache.py
@@ -7,6 +7,7 @@ from os.path import join, dirname, basename, splitext, exists
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 
+import app.models
 from ... import models
 
 
--- pandora/item/models.py
+++ pandora/item/models.py
@@ -668,7 +668,7 @@ class Item(models.Model):
         s.popularity = self.accessed.aggregate(Sum('accessed'))['accessed__sum']
         s.save()
         #update cached values in clips
-        self.clips.all().update(director=s.director, year=s.title)
+        self.clips.all().update(director=s.director, title=s.title)
 
     def update_facets(self):
         for key in self.facet_keys + ['title']:
