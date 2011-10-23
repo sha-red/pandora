48f078db5f1f33f5d51ccbf2d5c93629ed61b7ff
j <0x006A@0x2620.org>
2011-10-23 12:50:24 +0200

    update static files in update.sh, remove generated file

--- update.sh
+++ update.sh
@@ -3,7 +3,6 @@ base=`pwd`
 bzr pull http://code.0x2620.org/pandora/
 cd static/oxjs
 bzr pull http://code.0x2620.org/oxjs/
-echo build oxjs
-./tools/build/build.py >/dev/null
-cd $base
 test -e src/python-ox && cd src/python-ox && bzr pull http://code.0x2620.org/python-ox/
+cd $base
+cd pandora && ./manage.py update_static
