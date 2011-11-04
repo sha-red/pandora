ae6f8df8862a19f858ec53664b2fe9302b49d17d
j <0x006A@0x2620.org>
2011-11-04 12:29:00 +0100

    show sqldiff

--- update.sh
+++ update.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
+pandora_repos=http://code.0x2620.org/pandora/
+oxjs_repos=http://code.0x2620.org/oxjs/
+python_ox_repos=http://code.0x2620.org/python-ox/
+
 cd `dirname $0`
 base=`pwd`
 current=`bzr revno`
-bzr pull http://code.0x2620.org/pandora/
+bzr pull $pandora_repos
 new=`bzr revno`
-cd static/oxjs
-current=$current`bzr revno`
-bzr pull http://code.0x2620.org/oxjs/
-new=$new`bzr revno`
+cd $base
+if [ -e static/oxjs ]; then
+  cd static/oxjs
+  current=$current`bzr revno`
+  bzr pull $oxjs_repos
+  new=$new`bzr revno`
+else
+  cd static
+  bzr branch $oxjs_repos
+  cd oxjs
+  new=$new`bzr revno`
+fi
+
 cd $base
 if [ -e src/python-ox ]; then
   cd src/python-ox
   current=$current`bzr revno`
-  bzr pull http://code.0x2620.org/python-ox/
+  bzr pull $python_ox_repos
   new=$new`bzr revno`
 fi
 cd $base
@@ -20,4 +33,5 @@ if [ $current -ne $new ]; then
   cd pandora
   ./manage.py update_static
   ./manage.py compile_pyc
+  ./manage.py sqldiff -a | grep -v BEGIN | grep -v COMMIT
 fi
