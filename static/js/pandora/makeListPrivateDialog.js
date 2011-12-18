2569e589c76d2c67de1b9ef8ae4f01924ba8f2cf
j <0x006A@0x2620.org>
2011-12-18 09:30:35 +0000

    add makeListPrivateDialog.js

---
+++ static/js/pandora/makeListPrivateDialog.js
@@ -0,0 +1,50 @@
+'use strict';
+
+pandora.ui.makeListPrivateDialog = function(name, subscribers, callback) {
+
+    var that = Ox.Dialog({
+            buttons: [
+                Ox.Button({
+                    id: 'keep',
+                    title: 'Keep List Public'
+                }).bindEvent({
+                    click: function() {
+                        that.close();
+                        callback(false);
+                    }
+                }),
+                Ox.Button({
+                    id: 'make',
+                    title: 'Make List Private'
+                }).bindEvent({
+                    click: function() {
+                        that.close();
+                        callback(true);
+                    }
+                })
+            ],
+            content: Ox.Element()
+                .append(
+                    $('<img>')
+                        .attr({src: '/static/png/icon64.png'})
+                        .css({position: 'absolute', left: '16px', top: '16px', width: '64px', height: '64px'})
+                )
+                .append(
+                    $('<div>')
+                        .css({position: 'absolute', left: '96px', top: '16px', width: '192px'})
+                        .html(
+                            'Are you sure you want to make the list "'
+                            + name + '" private and lose its '
+                            + (subscribers == 1 ? 'subscriber' : subscribers + ' subscribers')
+                            + '?'
+                        )
+                ),
+            height: 128,
+            keys: {enter: 'make', escape: 'keep'},
+            title: 'Make List Private',
+            width: 304
+        })
+
+    return that;
+
+};
