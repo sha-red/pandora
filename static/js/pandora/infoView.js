416307f4ef20906c32abf9d773a7eb6b6c757f38
rolux <rolux@rollux.org>
2011-12-22 17:37:13 +0000

    misc bugfixes

--- static/js/pandora/infoView.js
+++ static/js/pandora/infoView.js
@@ -769,9 +769,10 @@ pandora.ui.infoView = function(data) {
         if (canEdit) {
             $rightsLevelSelect = Ox.Select({
                     items: pandora.site.rightsLevels.map(function(rightsLevel, i) {
-                        return {id: i, title: rightsLevel.name, checked: i == data.rightslevel};
+                        return {id: i, title: rightsLevel.name};
                     }),
-                    width: 128
+                    width: 128,
+                    value: data.rightslevel
                 })
                 .addClass('OxColor OxColorGradient')
                 .css({
@@ -781,7 +782,7 @@ pandora.ui.infoView = function(data) {
                 .data({OxColor: $rightsLevelElement.data('OxColor')})
                 .bindEvent({
                     change: function(event) {
-                        var rightsLevel = event.selected[0].id;
+                        var rightsLevel = event.value
                         $rightsLevelElement = getRightsLevelElement(rightsLevel);
                         $rightsLevelSelect
                             .css({background: $rightsLevelElement.css('background')})
