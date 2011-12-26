d070cb616b3954b30c2d23f7f1c97795bef55752
j <0x006A@0x2620.org>
2011-12-27 00:06:58 +0530

    pad ids to len 7 for storage

--- pandora/item/models.py
+++ pandora/item/models.py
@@ -772,6 +772,7 @@ class Item(models.Model):
 
     def path(self, name=''):
         h = self.itemId
+        h = (7-len(h))*'0' + h
         return os.path.join('items', h[:2], h[2:4], h[4:6], h[6:], name)
 
     '''
