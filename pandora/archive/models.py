2f914f16d6138791c4f479e8855da60302306de2
j <0x006A@0x2620.org>
2012-02-24 23:09:21 +0530

    add flv

--- pandora/archive/models.py
+++ pandora/archive/models.py
@@ -126,7 +126,7 @@ class File(models.Model):
                 self.pixels = int(self.width * self.height * float(utils.parse_decimal(self.framerate)) * self.duration)
 
         else:
-            self.is_video = os.path.splitext(self.path)[-1].lower() in ('.avi', '.mkv', '.dv', '.ogv', '.mpeg', '.mov', '.webm', '.mp4', '.mpg', '.wmv', '.mts')
+            self.is_video = os.path.splitext(self.path)[-1].lower() in ('.avi', '.mkv', '.dv', '.ogv', '.mpeg', '.mov', '.webm', '.mp4', '.mpg', '.wmv', '.mts', '.flv')
             self.is_audio = os.path.splitext(self.path)[-1].lower() in ('.mp3', '.wav', '.ogg', '.flac', '.oga', '.wma')
             self.is_subtitle = os.path.splitext(self.path)[-1].lower() in ('.srt', )
 
