2971dd2d40632cb0b4eb4841b245e39743e6032f
j <0x006A@0x2620.org>
2012-03-01 15:47:35 +0100

    clear more cache on metadata reload, fixes #636

--- static/js/pandora/infoView.js
+++ static/js/pandora/infoView.js
@@ -577,7 +577,8 @@ pandora.ui.infoView = function(data) {
         pandora.api.updateExternalData({
             id: ui.item
         }, function(result) {
-            Ox.Request.clearCache(item);
+            //reloading metadata might change results too(i.e. genre)
+            Ox.Request.clearCache();
             if (ui.item == item && ui.itemView == 'info') {
                 pandora.$ui.contentPanel.replaceElement(
                     1, pandora.$ui.item = pandora.ui.item()
