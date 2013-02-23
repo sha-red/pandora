d2a922bec41705286f685de1668dd6254a17e2ad
j <0x006A@0x2620.org>
2013-02-23 11:37:02 +0000

    provide hook to overwrite site.url in config in local_settings.py

--- pandora/app/config.py
+++ pandora/app/config.py
@@ -40,6 +40,8 @@ def load_config():
 
     if config:
         settings.SITENAME = config['site']['name']
+        if getattr(settings, 'SITEURL'):
+            config['site']['url'] = settings.SITEURL
         settings.URL = config['site']['url']
         settings.EMAIL_SUBJECT_PREFIX = '[%s]'%settings.SITENAME
         settings.DEFAULT_FROM_EMAIL = config['site']['email']['system']
