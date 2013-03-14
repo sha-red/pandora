fb873c8ddaba1831d1abb70b38406229a5c303a0
rolux <rolux@rolux.org>
2013-03-14 07:44:50 +0000

    update filmographies

--- static/js/pandora/infoView.indiancinema.js
+++ static/js/pandora/infoView.indiancinema.js
@@ -600,7 +600,7 @@ pandora.ui.infoView = function(data) {
             if (result.data.items) {
                 result.data.items.forEach(function(item) {
                     var year = item.year || 'Unknown Year';
-                    if (key == 'name') {
+                    if (key == 'name' && result.data.item.length > 1) {
                         item.roles = nameKeys.filter(function(nameKey) {
                             return Ox.contains(item[nameKey], value);
                         });
