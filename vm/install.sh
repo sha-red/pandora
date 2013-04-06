c0a5fccc040f7ee5d068d160ecba3c72d4429dfe
j <0x006A@0x2620.org>
2013-04-06 19:22:01 +0200

    pip 1.1 no longer supports -E

--- vm/install.sh
+++ vm/install.sh
@@ -3,5 +3,5 @@ CH="chroot $1"
 $CH bzr branch http://code.0x2620.org/pandora /srv/pandora
 $CH bzr branch http://code.0x2620.org/oxjs /srv/pandora/static/oxjs
 $CH virtualenv --system-site-packages /srv/pandora
-$CH pip -E /srv/pandora install -r /srv/pandora/requirements.txt
+$CH /srv/pandora/bin/pip install -r /srv/pandora/requirements.txt
 
