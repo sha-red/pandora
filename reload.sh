28ba382f11ec527f884781b40ab4d67ad7192524
j <0x006A@0x2620.org>
2013-04-18 15:26:52 +0000

    warn if reload is run as non root user

--- reload.sh
+++ reload.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+if [ `whoami` != 'root' ]; then
+    echo you have to be root or run with sudo
+    exit 1
+fi
 for service in pandora pandora-tasks pandora-encoding pandora-cron; do
     service $service reload
 done
