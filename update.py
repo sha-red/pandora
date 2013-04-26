dd72842dd1c7b1fcf8a76f93b72e92fc7d7a4291
j <0x006A@0x2620.org>
2013-04-26 13:29:34 +0000

    ./update.py dev fixes #1486

--- update.py
+++ update.py
@@ -116,7 +116,7 @@ if __name__ == "__main__":
             run('./manage.py', 'update_static')
             run('./manage.py', 'compile_pyc')
         elif not development:
-            print 'pan.do/ra is up to date, run "./update development" to update to the current development version'
+            print 'pan.do/ra is up to date, run "./update dev" to update to the current development version'
         diff = get('./manage.py', 'sqldiff', '-a').strip()
         if diff != '-- No differences':
             print 'Database has changed, please make a backup and run ./update.py database'
