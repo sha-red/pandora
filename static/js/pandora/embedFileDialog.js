a7cead82e81e80160e66b9d1d1437d1fdb208a7f
rolux <rolux@rolux.org>
2013-04-26 10:44:25 +0200

    add embedFileDialog

---
+++ static/js/pandora/embedFileDialog.js
@@ -0,0 +1,58 @@
+// vim: et:ts=4:sw=4:sts=4:ft=javascript
+'use strict';
+
+pandora.ui.embedFileDialog = function(id) {
+
+    var isImage = Ox.contains(['jpg', 'png'], selected.split('.').pop()),
+        url = 'http' + (pandora.site.site.https ? 's' : '') + '://'
+            + pandora.site.site.url + '/files/' + id,
+
+        $content = Ox.Element()
+            .css({margin: '16px'})
+            .html(
+                'To embed this file, use the following HTML:<br>'
+            ),
+
+        $embed = $('<textarea>')
+            .css({
+                width: '336px',
+                height: '64px',
+                marginTop: '8px',
+            })
+            .val(
+                isImage
+                ? '<img src="' + url + '">'
+                : '<a href="' + url + '"><img src="' + url + '.jpg"></a>'
+            )
+            .on({
+                click: function() {
+                    this.focus();
+                    this.select();
+                }
+            })
+            .appendTo($content),
+
+        that = Ox.Dialog({
+            buttons: [
+                Ox.Button({
+                    id: 'close',
+                    title: 'Close'
+                }).bindEvent({
+                    click: function() {
+                        that.close();
+                    }
+                })
+            ],
+            closeButton: true,
+            content: $content,
+            fixedSize: true,
+            height: 128,
+            keys: {escape: 'close'},
+            removeOnClose: true,
+            title: 'Embed File',
+            width: 368 
+        });
+
+    return that;
+
+};
