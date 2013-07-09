208a38c41966340c76394e56b1ba038ad626cadc
rolux <rolux@rollux.org>
2013-07-09 09:04:35 +0000

    load home screen first, and app panel upon home screen callback (allowing for animation without the app panel blocking rendering)

--- static/js/pandora.js
+++ static/js/pandora.js
@@ -318,16 +318,16 @@ appPanel
             videoFormat: Ox.UI.getVideoFormat(pandora.site.video.formats)
         });
 
-        // set up url controller
+        // set locale and initialize url controller
         pandora.setLocale(pandora.user.ui.locale, function() {
             pandora.URL.init().parse(function() {
+                var isHome = Ox.contains(['/', '/home'], document.location.pathname);
                 if (data.browserSupported) {
                     stopAnimation();
                     $('#loadingScreen').remove();
                 } else {
                     loadBrowserMessage();
                 }
-
                 Ox.Theme(pandora.user.ui.theme);
                 if (isEmbed) {
                     pandora.$ui.embedPanel = pandora.ui.embedPanel().display();
@@ -345,31 +345,37 @@ appPanel
                     });
                 } else if (isPrint) {
                     pandora.$ui.printView = pandora.ui.printView().display();
+                } else if (isHome) {
+                    pandora.$ui.home = pandora.ui.home().showScreen(initPandoraApp);
                 } else {
-                    pandora.$ui.appPanel = pandora.ui.appPanel().display();
-                    Ox.Request.requests() && pandora.$ui.loadingIcon.start();
-                    pandora.$ui.body.ajaxStart(pandora.$ui.loadingIcon.start);
-                    pandora.$ui.body.ajaxStop(pandora.$ui.loadingIcon.stop);
-                    Ox.Request.bindEvent({
-                        error: pandora.ui.errorDialog,
-                        request: function(data) {
-                            pandora.$ui.loadingIcon.options({
-                                tooltip: (data.requests || 'No')
-                                    + ' request'
-                                    + (data.requests == 1 ? '' : 's')
-                            });
-                        }
-                    });
-                    pandora.site.sectionButtonsWidth = pandora.$ui.sectionButtons.width() + 8;
-                    if (getLocalStorage('pandora.onload')) {
-                        try {
-                            eval(localStorage['pandora.onload'])
-                        } catch(e) {}
-                    }
+                    initPandoraApp();
                 }
             });
         });
 
+    }
+
+    function initPandoraApp() {
+        pandora.$ui.appPanel = pandora.ui.appPanel().display();
+        Ox.Request.requests() && pandora.$ui.loadingIcon.start();
+        pandora.$ui.body.ajaxStart(pandora.$ui.loadingIcon.start);
+        pandora.$ui.body.ajaxStop(pandora.$ui.loadingIcon.stop);
+        Ox.Request.bindEvent({
+            error: pandora.ui.errorDialog,
+            request: function(data) {
+                pandora.$ui.loadingIcon.options({
+                    tooltip: (data.requests || 'No')
+                        + ' request'
+                        + (data.requests == 1 ? '' : 's')
+                });
+            }
+        });
+        pandora.site.sectionButtonsWidth = pandora.$ui.sectionButtons.width() + 8;
+        if (getLocalStorage('pandora.onload')) {
+            try {
+                eval(localStorage['pandora.onload'])
+            } catch(e) {}
+        }
     }
 
     function loadBrowserMessage() {
