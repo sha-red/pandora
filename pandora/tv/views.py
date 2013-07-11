d1e6da97ade738b181f5727f5d109b4b4dd09077
j <0x006A@0x2620.org>
2013-07-11 18:07:32 +0000

    just ok

--- pandora/tv/views.py
+++ pandora/tv/views.py
@@ -32,7 +32,7 @@ def tv(request):
             response = json_response(status=404, text='list not found')
     else:
         channel, created = models.Channel.objects.get_or_create(list=None)
-        response = json_response(status=200, text='created')
+        response = json_response(status=200, text='ok')
         response['data'] = channel.json(request.user)
     return render_to_json_response(response)
 actions.register(tv, cache=False)
