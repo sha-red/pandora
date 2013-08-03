c35445f498aa81149be99e6dfd2a2cdab79b440f
rolux <rolux@rollux.org>
2013-08-03 14:35:28 +0000

    add timeout before reloading list after undo/redo

--- static/js/utils.js
+++ static/js/utils.js
@@ -393,7 +393,8 @@ pandora.createLinks = function($element) {
         }
         if (object.action != 'copy') {
             if (type == 'item' && ui.section == 'items' && Ox.contains(object.targets, ui._list)) {
-                pandora.reloadList();
+                // FIXME: Why is this timeout needed?
+                setTimeout(pandora.reloadList, 250);
             } else if (type == 'clip' && ui.section == 'edits' && Ox.contains(object.targets, ui.edit)) {
                 // FIXME: reload clip list
             }
