722b9fb4184eb76446260f81ebc4c7fa6d742f89
rolux <rolux@rollux.org>
2013-08-08 12:59:42 +0000

    add user and locale menu extra buttons

---
+++ static/js/userButton.js
@@ -0,0 +1,45 @@
+'use strict';
+
+pandora.ui.userButton = function() {
+
+    var isGuest = pandora.user.level == 'guest',
+        that = Ox.Element().css({marginLeft: '3px'});
+
+    $('<div>')
+        .addClass('OxLight')
+        .css({
+            float: 'left',
+            marginTop: '2px',
+            fontSize: '9px'
+        })
+        .html(
+            isGuest ? 'not signed in'
+            : Ox.encodeHTMLEntities(pandora.user.username)
+        )
+        .appendTo(that);
+
+    Ox.Button({
+            style: 'symbol',
+            title: 'user',
+            tooltip: Ox._(
+                isGuest ? 'Click to sign in, doubleclick to sign up'
+                : 'Click to open preferences, doubleclick to sign out'
+            ),
+            type: 'image'
+        })
+        .css({
+            float: 'left'
+        })
+        .bindEvent({
+            singleclick: function() {
+                pandora.UI.set({page: isGuest ? 'signin' : 'preferences'});
+            },
+            doubleclick: function() {
+                pandora.UI.set({page: isGuest ? 'signup' : 'signout'});
+            }
+        })
+        .appendTo(that);
+
+    return that;
+
+};
