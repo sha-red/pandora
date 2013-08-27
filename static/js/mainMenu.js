9f45aa2e2b07c7bf0d8a20d8c72dd4479578e5d3
rolux <rolux@rollux.org>
2013-08-27 14:30:51 +0000

    cosmetic changes

--- static/js/mainMenu.js
+++ static/js/mainMenu.js
@@ -207,7 +207,7 @@ pandora.ui.mainMenu = function() {
                 pandora.site.capabilities.canSeeDebugMenu[pandora.user.level]
                     ? [
                         { id: 'debugMenu', title: Ox._('Debug'), items: [
-                            { id: 'cache', title: Ox._((pandora.localStorage('enableCache') === false ? 'Enable' : 'Disable') + ' Cache')},
+                            { id: 'cache', title: Ox._((pandora.localStorage('enableCache') !== false ? 'Disable' : 'Enable') + ' Cache')},
                             { id: 'clearcache', title: Ox._('Clear Cache')},
                             {},
                             { id: 'debugmode', title: Ox._((pandora.localStorage('enableDebugMode') ? 'Disable' : 'Enable') + ' Debug Mode') },
