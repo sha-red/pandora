4b575c2c891c0fd7d8f0feda0e2844a5164a008a
j <0x006A@0x2620.org>
2013-09-26 15:27:43 +0000

    fix ./manage.py orphaned_media

--- pandora/archive/management/commands/orphaned_media.py
+++ pandora/archive/management/commands/orphaned_media.py
@@ -4,17 +4,18 @@ from django.core.management.base import BaseCommand
 from django.conf import settings
 import os
 from glob import glob
-from ... import models
 
+import app.monkey_patch
+from ... import models
 
 class Command(BaseCommand):
     """
     """
-    help = 'list files no longer without db entry'
+    help = 'list files without db entry'
     args = ''
 
     def handle(self, **options):
-        for f in glob(os.path.join(settings.MEDIA_ROOT, 'files', '*', '*', '*', '*')):
+        for f in glob(os.path.join(settings.MEDIA_ROOT, 'media', '*', '*', '*', '*')):
             oshash = f[-19:].replace('/', '')
             if models.File.objects.filter(oshash=oshash).count() == 0:
                 print f
