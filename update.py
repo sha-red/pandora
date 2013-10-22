89962a5dd4646c4d8d79d80f9f3459933c4a0886
j <0x006A@0x2620.org>
2013-10-22 20:00:51 +0000

    fix postupdate

--- update.py
+++ update.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
         diff = get('./manage.py', 'sqldiff', '-a').strip()
         if diff != '-- No differences':
             print 'Database has changed, please make a backup and run ./update.py db'
-        elif current != new:
+        if current != new:
             if pandora_old_revno != pandora_new_revno:
                 os.chdir(base)
                 run('./update.py', 'postupdate', pandora_old_revno, pandora_new_revno)
