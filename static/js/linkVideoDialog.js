70d362e655d65eb1fe632a383dbacfea412a3673
rolux <rolux@rolux.org>
2014-06-06 14:20:49 +0300

    add linkVideoDialog

---
+++ static/js/linkVideoDialog.js
@@ -0,0 +1,55 @@
+// vim: et:ts=4:sw=4:sts=4:ft=javascript
+'use strict';
+
+pandora.ui.linkVideoDialog = function() {
+
+    var ui = pandora.user.ui,
+
+        url = '/' + ui.item + '/480p' + ui.videoPoints[ui.item]['in'] + '.jpg',
+
+        $content = Ox.Element()
+            .css({margin: '16px'})
+            .html(Ox._('To link to this clip, use the following HTML:')),
+
+        $embed = $('<textarea>')
+            .css({
+                width: '322px',
+                height: '64px',
+                marginTop: '8px'
+            })
+            .val(
+                '<a href="' + document.location.href
+                + '"><img src="' + url + '"></a>'
+            )
+            .on({
+                click: function() {
+                    this.focus();
+                    this.select();
+                }
+            })
+            .appendTo($content),
+
+        that = Ox.Dialog({
+            buttons: [
+                Ox.Button({
+                    id: 'close',
+                    title: Ox._('Close')
+                }).bindEvent({
+                    click: function() {
+                        that.close();
+                    }
+                })
+            ],
+            closeButton: true,
+            content: $content,
+            fixedSize: true,
+            height: 128,
+            keys: {escape: 'close'},
+            removeOnClose: true,
+            title: Ox._('Link to Video'),
+            width: 368 
+        });
+
+    return that;
+
+};
