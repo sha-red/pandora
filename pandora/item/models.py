97d8344822db58db8af51c19f63c61979f07561b
j <0x006A@0x2620.org>
2014-11-19 16:54:45 +0000

    turn <br> into space

--- pandora/item/models.py
+++ pandora/item/models.py
@@ -1009,7 +1009,7 @@ class Item(models.Model):
     def update_layer_facet(self, key):
         current_values = [a['value']
             for a in self.annotations.filter(layer=key).distinct().values('value')]
-        current_values = [ox.decode_html(ox.strip_tags(v)) for v in current_values]
+        current_values = [ox.decode_html(ox.strip_tags(v.replace('<br>', ' '))) for v in current_values]
         saved_values = [i.value for i in Facet.objects.filter(item=self, key=key)]
         removed_values = filter(lambda i: i not in current_values, saved_values)
         if removed_values:
