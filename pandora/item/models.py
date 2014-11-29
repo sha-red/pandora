8acfd57d1088a040d6e676db38e0a93b81088902
j <0x006A@0x2620.org>
2014-11-29 14:40:56 +0100

    dont expect video in file info

--- pandora/item/models.py
+++ pandora/item/models.py
@@ -1098,7 +1098,7 @@ class Item(models.Model):
             if stream.duration + offset < position:
                 offset += stream.duration
             else:
-                if not stream.file.is_video or not stream.file.info['video']:
+                if not stream.file.is_video or not stream.file.info.get('video'):
                     return None
                 position = position - offset
                 if not height:
