3f83a539e49d1d8756be1f32d7c3a94154659cdd
rolux <rolux@rolux.org>
2014-12-18 14:27:36 +0000

    update api docs parser

--- static/js/apiDialog.js
+++ static/js/apiDialog.js
@@ -145,7 +145,7 @@ pandora.ui.apiDialog = function() {
             $doc.find('.Ox' + type).removeClass('Ox' + type);
         });
         $doc.html(
-            $doc.html().replace(/X(\w+)X/g, '<b>$1</b>')
+            $doc.html().replace(/BOLD(\w+)BOLD/g, '<b>$1</b>')
         );
         parts = $doc.html().split('<b>notes</b>');
         if (parts.length == 2) {
