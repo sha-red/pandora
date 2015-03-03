5ce83cbe638a836f9aec0b124a035a92d37d058b
j <0x006A@0x2620.org>
2015-03-03 08:17:48 +0000

    disable Manage Entities... button if no entities are defined. fixes #2704

--- static/js/documentsDialog.js
+++ static/js/documentsDialog.js
@@ -9,6 +9,7 @@ pandora.ui.documentsDialog = function() {
 
         // FIXME: user may not have the manage entites capability
         $switchButton = Ox.Button({
+                disabled: !pandora.site.entities.length || !pandora.site.capabilities.canManageEntities[pandora.user.level],
                 title: Ox._('Manage Entities...')
             })
             .bindEvent({
