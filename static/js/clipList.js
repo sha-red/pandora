e063cc702061fd701c73b5e65de4bf366879d6c0
j <0x006A@0x2620.org>
2015-05-23 18:02:24 +0200

    use empty string if value is undefined or null, fixes #2790

--- static/js/clipList.js
+++ static/js/clipList.js
@@ -44,7 +44,9 @@ pandora.ui.clipList = function(videoRatio) {
                         + Ox.formatDuration(data.out);
                 } else {
                     format = pandora.getSortKeyData(sortKey).format;
-                    if (format) {
+                    if (Ox.isUndefined(data[sortKey]) || Ox.isNull(data[sortKey])) {
+                        info = '';
+                    } else if (format) {
                         info = (
                             /^color/.test(format.type.toLowerCase()) ? Ox.Theme : Ox
                         )['format' + Ox.toTitleCase(format.type)].apply(
