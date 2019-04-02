4ec7e1b6c1585adb7fd64d40d9d9a0ea2c0739f2
j <j@mailb.org>
2019-04-02 14:57:35 +0800

    fix regexp

--- static/js/infoView.indiancinema.js
+++ static/js/infoView.indiancinema.js
@@ -614,10 +614,10 @@ pandora.ui.infoView = function(data, isMixed) {
         if (/\d{2}-\d{2}-\d{4}/.test(value)) {
             value = Ox.reverse(value.split('-')).join('-')
         }
-        if (/\d{4}/\d{2}/\d{d}/.test(value)) {
+        if (/\d{4}i\/\d{2}\/\d{d}/.test(value)) {
             value = value.split('/').join('-')
         }
-        if (/\d{2}/\d{2}/\d{4}/.test(value)) {
+        if (/\d{2}\/\d{2}\/\d{4}/.test(value)) {
             value = Ox.reverse(value.split('/')).join('-')
         }
         return value
