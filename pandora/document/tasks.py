ca7741f92c65a98db1aff54e01f66555215efc2e
j <j@mailb.org>
2020-09-11 13:55:01 +0200

    run bulk update as task

--- pandora/document/tasks.py
+++ pandora/document/tasks.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+import ox
 from celery.task import task
 
 @task(queue="encoding")
@@ -6,3 +6,20 @@ def extract_fulltext(id):
     from . import models
     d = models.Document.objects.get(id=id)
     d.update_fulltext()
+
+
+@task(queue='default')
+def builk_edit(data, username):
+    from django.db import transaction
+    from . import models
+    from item.models import Item
+    user = models.User.objects.get(username=username)
+    item = 'item' in data and Item.objects.get(public_id=data['item']) or None
+    documents = models.Document.objects.filter(pk__in=map(ox.fromAZ, data['id']))
+    for document in documents:
+        if document.editable(user, item):
+            with transaction.atomic():
+                document.refresh_from_db()
+                document.edit(data, user, item)
+                document.save()
+    return {}
