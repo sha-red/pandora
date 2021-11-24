e237d081201424c1f1f0f83f63718c5c922d3986
j <j@mailb.org>
2021-11-24 14:23:50 +0100

    fix sort by value of smart edits

--- pandora/edit/views.py
+++ pandora/edit/views.py
@@ -190,7 +190,7 @@ def _order_clips(edit, sort):
             'in': 'start',
             'out': 'end',
             'text': 'sortvalue',
-            'volume': 'sortvolume',
+            'volume': 'volume' if edit.type == 'smart' else 'sortvolume',
             'item__sort__item': 'item__sort__public_id',
         }.get(key, key)
         order = '%s%s' % (operator, key)
