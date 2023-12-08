74fca45a9f0a923bfca72806bbcb42bc101c077a
j <j@mailb.org>
2023-12-08 16:17:21 +0000

    use /etc/apt/trusted.gpg.d/ instead of apt-key add

--- vm/install_elasticsearch.sh
+++ vm/install_elasticsearch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-curl -sL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+curl -sL https://artifacts.elastic.co/GPG-KEY-elasticsearch > /etc/apt/trusted.gpg.d/elasticsearch.asc
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elasticsearch.list
 apt-get update -qq
 apt-get -y install elasticsearch
